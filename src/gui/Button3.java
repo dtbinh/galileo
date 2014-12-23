ba9befee35c6271da2003762b4644003fc63964a
rschl <rschlott@hotmail.de>
2014-12-23 11:48:00 +0100

    changed gui

--- src/gui/Button3.java
+++ src/gui/Button3.java
@@ -1,27 +1,32 @@
 package gui;
-
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
- 
-public class Button3 {
-    
-   private JFrame mainFrame;
-   private JLabel statusLabel;
-   private JPanel controlPanel;
 
+import javax.swing.*;
+
+import movement.Movements;
+import network.Net;
+import network.NetSettings;
+
+public class Button3 {
+	//Die einzelteile der GUI vorbereiten.
+   private JFrame mainFrame;
+   
+   private JPanel controlPanel;
+   JTextField eingabe = new JTextField();
+   
    public Button3(){
       prepareGUI();
    }
-
+   // GUI starten.
    public static void main(String[] args){
 	   Button3  ButtonCreate = new Button3();      
 	   ButtonCreate.showButton();
    }
-
+   //Die GUI-Fenster-Eigenschaften angeben.  
    private void prepareGUI(){
       mainFrame = new JFrame("Remotecontrol");
-      mainFrame.setSize(400,400);
+      mainFrame.setSize(450,450);
       mainFrame.setLayout(new GridLayout(3, 1));
       mainFrame.addWindowListener(new WindowAdapter() {
          public void windowClosing(WindowEvent windowEvent){
@@ -29,69 +34,95 @@ public class Button3 {
          }        
       });    
               
-      statusLabel = new JLabel("",JLabel.CENTER);    
-
-      statusLabel.setSize(350,50);
-
+      
+      
       controlPanel = new JPanel();
+      
+      
       controlPanel.setLayout(null);
-
+      controlPanel.setBounds(0, 0, 450, 450);
       
       mainFrame.add(controlPanel);
-      mainFrame.add(statusLabel);
+      
       mainFrame.setVisible(true);  
    }
-    
+   
+   
    private void showButton(){
-
+	   //Die Button Vorbereiten.
       JButton Forward = new JButton("Forward");        
       JButton Backward = new JButton("Backward");
       JButton Right = new JButton("Right");
       JButton Left = new JButton("Left");
+      JButton RotateRight = new JButton("Rotate Right");
+      JButton RotateLeft = new JButton("Rotate Left");
+      JButton Restart_map = new JButton("Restart_map");
       
-      JButton Restart_map = new JButton("Restart Map");
-      
-      Left.setHorizontalTextPosition(SwingConstants.LEFT);   
-
+     
+      //Die Funktionen der Buttons erstellen.
       Forward.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
-            statusLabel.setText("Forward");
+        	 Net.sendRobotCmd(NetSettings.getEv2Ip(), NetSettings.getRobotPort(), 100+ Integer.parseInt(eingabe.getText()));
          }          
       });
 
       Backward.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
-            statusLabel.setText("Backward");
+        	 Net.sendRobotCmd(NetSettings.getEv2Ip(), NetSettings.getRobotPort(), 200+ Integer.parseInt(eingabe.getText()));
          }
       });
 
       Right.addActionListener(new ActionListener() {
           public void actionPerformed(ActionEvent e) {
-             statusLabel.setText("Right");
+        	  Net.sendRobotCmd(NetSettings.getEv2Ip(), NetSettings.getRobotPort(), 500+ Integer.parseInt(eingabe.getText()));
           }
        });
       
       Left.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
-            statusLabel.setText("Left");
+        	 Net.sendRobotCmd(NetSettings.getEv2Ip(), NetSettings.getRobotPort(), 300+ Integer.parseInt(eingabe.getText()));
          }
       });
       
-      Restart_map.addActionListener(new ActionListener() {
+      RotateLeft.addActionListener(new ActionListener() {
           public void actionPerformed(ActionEvent e) {
-             statusLabel.setText("Restart_map");
+         	 
           }
        });
+      
+      RotateRight.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+         	
+          }
+       });
+      
+      Restart_map.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+             
+          }
+       });
+      //Die einzelnen Elemente die in der GUI enthalten sein sollen einsetzen.
       controlPanel.add(Forward);
       controlPanel.add(Backward);
       controlPanel.add(Right);
       controlPanel.add(Left);
-      controlPanel.add(Restart_map); 
+      controlPanel.add(RotateRight);
+      controlPanel.add(RotateLeft);
+      controlPanel.add(Restart_map);
+      controlPanel.add(eingabe);
+     
+      
+      //Die Positionen der jeweiligen GUI-Elemente festlegen.
       Forward.setBounds(150, 0, 100, 25);
       Backward.setBounds(150, 50, 100, 25);
       Right.setBounds(200, 25, 100, 25);
       Left.setBounds(100, 25, 100, 25);
-      Restart_map.setBounds(140, 100, 120, 25);
+      RotateRight.setBounds(300, 25, 100, 25);
+      RotateLeft.setBounds(0, 25, 100, 25);
+      Restart_map.setBounds(140, 100, 100, 25);
+      eingabe.setBounds(150, 75, 100, 25);
+      
+      
       mainFrame.setVisible(true);  
    }
 }
