f952362e183f8f12ee859d92029338b24d4447d6
LaPush <frostbeule10000@msn.com>
2015-01-12 15:08:50 +0100

    new testing class to test new test scenario

--- src/mapping/MapTestingClass.java
+++ src/mapping/MapTestingClass.java
@@ -16,17 +16,17 @@ public class MapTestingClass {
 	public MapTestingClass() {
 
 		TestVector vector1 = new TestVector(5,0);
-		vector1.x = 5;
-		vector1.y = 0;
+//		vector1.x = 5;
+//		vector1.y = 0;
 		TestVector vector2 = new TestVector(0,5);
-		vector2.x = 0;
-		vector2.y = 5;
-		TestVector vector3 = new TestVector(-5,0);
-		vector3.x = -5;
-		vector3.y = 0;
-		TestVector vector4 = new TestVector(0, -5);
-		vector4.x = 0;
-		vector4.y = -5;
+//		vector2.x = 0;
+//		vector2.y = 5;
+		TestVector vector3 = new TestVector(-6,0);
+//		vector3.x = -5;
+//		vector3.y = 0;
+		TestVector vector4 = new TestVector(0,-6);
+//		vector4.x = 0;
+//		vector4.y = -5;
 		map3.updateMapFromVectors(vector1);
 		map3.updateMapFromVectors(vector2);
 		map3.updateMapFromVectors(vector3);
@@ -34,7 +34,7 @@ public class MapTestingClass {
 		MapObject wall = MapObject.WALL;
 		MapObject empty = MapObject.EMPTY;
 		MapObject obstacle = MapObject.OBSTACLE;
-		map3.get(2).set(2, obstacle);
+//		map3.get(2).set(2, obstacle);
 
 		MapObject[] mapObjects = new MapObject[20];
 		ArrayList<MapObject> mapObjectsList = new ArrayList<MapObject>();
